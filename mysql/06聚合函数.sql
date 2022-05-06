d51019d32cae09602050ca8409ac337fac22ab14
归故里 <97590219+13870517674@users.noreply.github.com>
2022-05-06 10:05:10 +0800

    Add files via upload

---
+++ mysql/06聚合函数.sql
@@ -0,0 +1,16 @@
+-- 聚合函数
+-- 1.统计该企业员工数量
+select count(*) from emp;
+select count(idcard) from emp;  -- null值不参与聚合函数运算
+
+-- 2.统计该企业员工的平均年龄
+select avg(age) from emp;
+
+-- 3.统计该企业员工的最大年龄
+select max(age) from emp;
+
+-- 4.统计该企业员工的最小年龄
+select min(age) from emp;
+
+-- 5.统计北京地区员工的年龄之和
+select sum(age) from emp where workaddress ='北京';
