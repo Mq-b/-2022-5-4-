3803a699c90b73bb2e72c37a4776ecb658e5c30e
归故里 <97590219+13870517674@users.noreply.github.com>
2022-05-08 22:57:18 +0800

    Delete 106main.py

--- 106main.py
+++
@@ -1,6 +0,0 @@
-import bmi                     # 导入bmi模块
-bmi.fun_bmi("尹一伊",175,120)  # 执行模块中的fun_bmi()方法，也就是函数
-
-# 在导入模块时设置别名的应用
-# import bmi as m                     # 导入bmi模块
-# m.fun_bmi("尹一伊",175,120)  # 执行模块中的fun_bmi()方法
