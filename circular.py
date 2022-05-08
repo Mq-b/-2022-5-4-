617b03f139473f4aba5c10b3db12051c943a6d94
归故里 <97590219+13870517674@users.noreply.github.com>
2022-05-08 22:57:32 +0800

    Delete circular.py

--- circular.py
+++
@@ -1,8 +0,0 @@
-import math
-PI=math=math.pi
-def girth(r):
-    return round(2*PI*r,2)      #计算圆周长并保留两位小数，round函数就是保留小数的
-def area(r):
-    return round(PI*r**2,2)     #计算圆面积并保留两位小数
-if __name__=='__main__':
-    print(girth(10))
