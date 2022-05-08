ee0670e9891dbc884d19c2b1be305d42abb88b0a
归故里 <97590219+13870517674@users.noreply.github.com>
2022-05-08 22:57:25 +0800

    Delete compute.py

--- compute.py
+++
@@ -1,5 +0,0 @@
-import rectangle as r      #导入矩形模块
-import circular as c       #导入圆形模块
-if __name__=='__main__':   #程序从这里开始执行 可以有多个模块 , 但只有一个文件里的__name__为main
-    print('圆的周长为:',c.girth(10))
-    print('矩形的周长为:',r.girth(10,20))
