9308e5b86a38fe39f1912f53779eb6cff4d7234c
归故里 <97590219+13870517674@users.noreply.github.com>
2022-05-12 12:08:24 +0800

    Update C.h
    更改

--- C++/extern"C"/C.h
+++ C++/extern"C"/C.h
@@ -1,6 +1,8 @@
-#pragma once;       //防止头文件重复包含，Windows平台专属
+#pragma once;       //防止头文件重复包含
 #ifdef __cplusplus //使用预定义宏判断是否为cpp文件
 extern "C" {
+#endif
     void display();
+#ifdef __cplusplus 
 }
 #endif
