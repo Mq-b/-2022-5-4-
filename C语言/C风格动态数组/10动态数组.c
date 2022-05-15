c7a66237d010a0e60998735c7f5dd8c3cff87b64
归故里 <97590219+13870517674@users.noreply.github.com>
2022-05-15 13:51:46 +0800

    Update 10动态数组.c

--- C语言/C风格动态数组/10动态数组.c
+++ C语言/C风格动态数组/10动态数组.c
@@ -5,20 +5,20 @@
 #include"DynamicArray.h"
 void test01()
 {
-    //��ʼ����̬����
+    //初始化动态数组
     Dynamic_Array* myArray = Array_Init();
-    //����Ԫ��
-    printf("��̬��������%d\n", Cappcity_Array(myArray));
-    printf("��̬����Ԫ�ظ���%d\n", Size_Array(myArray));
+    //插入元素
+    printf("动态数组容量%d\n", Cappcity_Array(myArray));
+    printf("动态数组元素个数%d\n", Size_Array(myArray));
     for (int i = 0; i < 10; i++) {
         Push_Back_Array(myArray, i);
     }
-    printf("��̬��������%d\n", Cappcity_Array(myArray));
-    printf("��̬����Ԫ�ظ���%d\n", Size_Array(myArray));
-    //��ӡ
+    printf("动态数组容量%d\n", Cappcity_Array(myArray));
+    printf("动态数组元素个数%d\n", Size_Array(myArray));
+    //打印
     Print_Array(myArray);
-    printf("����%d\n", Find_Array(myArray, 2));
-    printf("ɾ��\n");
+    printf("查找%d\n", Find_Array(myArray, 2));
+    printf("删除\n");
     RemoveByValue_Array(myArray, 9);
     for (int i = 0; i < myArray->size; i++) {
         printf("%d\n", myArray->pAddr[i]);
@@ -31,7 +31,7 @@ void test01()
     for (int i = 0; i < myArray->size; i++) {
         printf("%d\n", myArray->pAddr[i]);
     }
-    //����
+    //销毁
     FreeSpace_Array(myArray);
 }
 int main()
