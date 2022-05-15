775db81ba577ea1c7a484e438ec2ee70efab62a2
归故里 <97590219+13870517674@users.noreply.github.com>
2022-05-15 09:45:04 +0800

    空格

--- mysql/04数据准备，表基本查询.sql
+++ mysql/04数据准备，表基本查询.sql
@@ -5,7 +5,7 @@ create table emp(
     id          int                 comment '编号',
     workno      varchar(10)         comment '工号',
     name        varchar(10)         comment '姓名',
-    gender      char(1)                comment '性别',
+    gender      char(1)             comment '性别',
     age         tinyint unsigned    comment '年龄',
     idcard      varchar(18)         comment '身份证号',
     workaddress varchar(50)         comment '工作地址',
