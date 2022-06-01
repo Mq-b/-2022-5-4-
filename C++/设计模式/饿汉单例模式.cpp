2d26606db49c8707c27151456d40633c68e215ad
归故里 <97590219+13870517674@users.noreply.github.com>
2022-06-01 20:13:31 +0800

    cpp 懒汉单例模式

---
+++ C++/设计模式/饿汉单例模式.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+class Test {
+public:
+	int num;
+	static Test* t;
+private:
+	Test()=default;
+	Test(Test& a) = delete;
+};
+Test* Test::t = new Test;
+
+class Demo {
+	Demo();
+};
+int main() {
+	Test* T = Test::t;
+	Test* T2 = Test::t;
+	std::cout << (T == T2) << std::endl;
+	T->num = 10;
+	std::cout << T->num << ',' << T2->num << std::endl;
+	return 0;
+}
