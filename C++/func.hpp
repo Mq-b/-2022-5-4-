4cb249581d9369a390366462ae14aa160883bd6f
归故里 <97590219+13870517674@users.noreply.github.com>
2022-07-11 13:56:25 +0800

    线程安全队列

--- C++/func.hpp
+++ C++/func.hpp
@@ -939,4 +939,103 @@ public:
 		return data.empty();
 	}
 };
+//包装thread
+class joining_thread {
+	std::thread t;
+public:
+	joining_thread()noexcept = default;
+	template<typename Callable,typename ...Args>
+	explicit joining_thread(Callable&& func,Args&&...args):t{std::forward<Callable>(func),std::forward<Args>(args)...}
+	{}
+	joining_thread(joining_thread&& other)noexcept :t{ std::move(other.t) }
+	{}
+	joining_thread& operator=(joining_thread&& other)noexcept {
+		if (t.joinable()) 
+			t.join();
+		t = std::move(other.t);
+		return *this;
+	}
+	joining_thread& operator=(std::thread other)noexcept {
+		if (t.joinable())
+			t.join();
+		t = std::move(other);
+		return *this;
+	}
+	~joining_thread()noexcept {
+		if (t.joinable())
+			t.join();
+	}
+	void swap(joining_thread& other)noexcept {
+		t.swap(other.t);
+	}
+	std::thread::id get_id()const noexcept {
+		return t.get_id();
+	}
+	bool joinable()const noexcept {
+		return t.joinable();
+	}
+	void join() {
+		t.join();
+	}
+	void detach() {
+		t.detach();
+	}
+	std::thread& as_thread()noexcept {
+		return t;
+	}
+	const std::thread& as_thread()const noexcept {
+		return t;
+	}
+};
+//线程安全队列
+template<typename T>
+class threadsafe_queue {
+private:
+	mutable std::mutex mut;
+	std::queue<T>data_queue;
+	std::condition_variable data_cond;
+public:
+	threadsafe_queue(){}
+	threadsafe_queue(threadsafe_queue const& other) {
+		std::lock_guard<std::mutex>lk(other.mut);
+	}
+	void push(T new_value) {
+		std::lock_guard<std::mutex>lk(mut);
+		data_queue.push(new_value);
+		data_cond.notify_one();
+	}
+	void wait_and_pop(T& value) {
+		std::unique_lock<std::mutex>lk(mut);
+		data_cond.wait(lk, [this] {return !data_queue.empty(); });
+		value = data_queue.front();
+		data_queue.pop();
+	}
+	std::shared_ptr<T> wait_and_pop() {
+		std::unique_lock<std::mutex>lk(mut);
+		data_cond.wait(lk, [this] {return !data_queue.empty(); });
+		std::shared_ptr<T>res(std::make_shared<T>(data_queue.front()));
+		data_queue.pop();
+		return res;
+	}
+	bool try_pop(T& value) {
+		std::lock_guard<std::mutex>lk(mut);
+		if (data_queue.empty())
+			return false;
+		value = data_queue.front();
+		data_queue.pop();
+		return true;
+	}
+	std::shared_ptr<T>try_pop() {
+		std::lock_guard<std::mutex>lk(mut);
+		if (data_queue.empty())
+			return std::shared_ptr<T>();
+		std::shared_ptr<T>res(std::make_shared<T>(data_queue.front()));
+		data_queue.pop();
+		return res;
+	}
+	bool empty()const {
+		std::lock_guard<std::mutex>lk(mut);
+		return data_queue.empty();
+	}
+};
 #endif
